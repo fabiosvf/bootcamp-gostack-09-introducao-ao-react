a2e3f8f474dc66407a3bc8cb8a8f2872ab799e4d
Fabio Silva Ferreira <fabiosvf@gmail.com>
2019-12-14 00:44:28 -0300

    Módulo 'Introdução ao React', Vídeo 'Estado & Imutabilidade'

--- src/componentes/TechList.js
+++ src/componentes/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       "ReactJS",
@@ -9,15 +10,36 @@ class TechList extends Component {
     ]
   };
 
-  render() {
-    console.log(this.state);
+  handleInputChange = e =>  {
+    this.setState({ newTech: e.target.value });
+  }
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+
+    console.log(this.state.newTech);
+  }
+
+  render() {
     return(
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={this.state.newTech} 
+        />
+        <button type="submit">Enviar</button>
+        </form>
+      </>
     );
   }
 }
