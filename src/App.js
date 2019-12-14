75c7db1ff250c0ef640f517b17b0668befcf6993
Fabio Silva Ferreira <fabiosvf@gmail.com>
2019-12-14 00:21:37 -0300

    Módulo 'Introdução ao React', Vídeo 'Class Components'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/Foto.jpg';
+import TechList from './componentes/TechList';
 
 function App() {
-  return <img width="200" src={profile} />
+  return <TechList />
 }
 
 export default App;
